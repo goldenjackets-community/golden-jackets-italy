759a4034b63fcc22bc6de9d5f5efd82f6d2d7672
Ricardo Gulias <gulias@DAREDE-001430.darede.corp>
2026-06-02 11:49:14 -0300

    refactor: rebuild Italy site from France template
    - Full template with all features (apply form with photo, FAQ, sponsors, etc)
    - 20 Italian regions in dropdown
    - Italian tricolore colors (green-white-red)
    - Luca D'Addeo as Chapter Leader (Novara, Piemonte)
    - Counter Lambda URL configured
    - GDPR privacy policy
    - All France references removed

---
+++ config.js
@@ -0,0 +1,5 @@
+var GJ_CONFIG = {
+  CHAPTER: 'italy',
+  ADMIN_EMAILS: ['ricardo.gulias@goldenjacketsbrazil.com', 'luca.daddeo@gmail.com'],
+  ADMIN_API: 'https://kqiq2bltjd.execute-api.us-east-1.amazonaws.com/admin'
+};
